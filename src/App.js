d780d8a2c27606ac41a1414cc103c27292169e6d
Mathias Medina Sanchez <mathiasmedinasanchez11@gmail.com>
2023-03-25 20:03:27 -0300

    build fix

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import {  ThemeProvider, createTheme } from '@mui/material';
+import theme from './theme';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import FamilyTree from './pages/FamilyTree';
+
+const createdTheme = createTheme({
+  palette: {
+    primary: {
+      main: theme.colors.siamoGreen
+    }
+  }
+});
+const App = () => {
+
+  return (
+      <ThemeProvider theme={createdTheme}>
+       <FamilyTree />
+      </ThemeProvider>
+  )
+}
+
+
+export default App;
+
